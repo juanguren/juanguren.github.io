8154e686d9200640af8e399bb1df4c21095077dd
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2018-12-01 21:37:05 -0500

    Toggle
    Toggle functions for Spotify and 500px embeds.

--- js/content.js
+++ js/content.js
@@ -1,8 +1,30 @@
+
+window.onload = function() {
+
+  document.getElementById('spotify').style.display = 'none';
+  document.getElementById('px_photo').style.display = 'none';
+
+};
+
 function mostrar_spot(){
 
+	var a = document.getElementById("spotify");
+	if (a.style.display === "none")
+{
+		a.style.display = "block";
+}	else {
+		a.style.display = "none";
+	}
 }
 
 function mostrar_px(){
 
+	var b = document.getElementById("px_photo");
+	if (b.style.display === "none")
+{
+		b.style.display = "block";
+}	else {
+		b.style.display = "none";
+	}
 }
 
