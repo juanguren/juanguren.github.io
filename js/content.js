aa10e60808f137bb2a0e10e925e380112d8677f8
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2019-03-19 21:05:17 -0500

    No slide
    Deleted the slide script

--- js/content.js
+++ js/content.js
@@ -6,23 +6,6 @@ window.onload = function() {
   document.getElementById('px_photo').style.display = 'none';
 };
 
-
-function carousel() {
-
-	var myIndex = 0;
-      carousel();
-      
-        var i;
-        var x = document.getElementsByClassName("slides");
-        for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";  
-        }
-        myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
-        x[myIndex-1].style.display = "block";  
-        setTimeout(carousel, 2500);
-      }
-
 function mostrar_spot(){
 
 	var sp = document.getElementById("spotify");
