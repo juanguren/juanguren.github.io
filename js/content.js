0ca20b135844e9be531b25b6a6b78392795fa0d4
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-02-27 19:31:37 -0500

    Update content.js
    Margins and timing of function

--- js/content.js
+++ js/content.js
@@ -6,17 +6,17 @@ window.onload = function() {
 };
 
 function mostrar_spot(){
-var sp = document.getElementById("spotify");
-	if (sp.style.display === "none")
-{
-	sp.style.display = "block";
-}	else {
-	sp.style.display = "none";
+	var sp = document.getElementById("spotify");
+
+	if (sp.style.display === "none") {
+		sp.style.display = "block";
+	} else{
+		sp.style.display = "none";
 	}
-	}	
+}
 
 function show_text() {
 	document.getElementById("show").style.display = "inline"; 
 }
 
-setTimeout("show_text()",13000);
+setTimeout("show_text()", 7000);
