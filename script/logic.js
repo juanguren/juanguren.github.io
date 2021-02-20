ed683214cc3c548f866dfc642ce1720d7f785b23
juanguren <juan_fe_7@live.com>
2021-02-20 17:08:46 -0500

    Navbar toggle logic. No transitions.

--- script/logic.js
+++ script/logic.js
@@ -1,4 +1,17 @@
 
+const responsiveNavBar = () =>{
+    const nav = document.getElementById('hamburguer-icon');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    nav.addEventListener('click', () =>{
+        mobileMenu.style.display === "none"
+         ? mobileMenu.style.display = "block"
+         : mobileMenu.style.display = "none";
+    });
+}
+
+responsiveNavBar();
+
 const wordChangeinStart = () =>{
     let domSpace = document.getElementById("for-array");
     let counter = 0;
