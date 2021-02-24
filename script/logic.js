128beb6dce1487a0e9e7fdb407cc76a7304ca4ed
juanguren <juan_fe_7@live.com>
2021-02-23 20:47:43 -0500

    Responsive navigation icon

--- script/logic.js
+++ script/logic.js
@@ -3,10 +3,18 @@ const responsiveNavBar = () =>{
     const nav = document.getElementById('hamburguer-icon');
     const mobileMenu = document.getElementById('mobile-menu');
 
+    const bar1 = document.getElementsByClassName('bar1')[0];
+    const bar2 = document.getElementsByClassName('bar2')[0];
+    const bar3 = document.getElementsByClassName('bar3')[0];
+
     nav.addEventListener('click', () =>{
         mobileMenu.style.display === "none"
          ? mobileMenu.style.display = "block"
          : mobileMenu.style.display = "none";
+        
+        bar1.classList.toggle('transformBar1'); 
+        bar2.classList.toggle('disappear');
+        bar3.classList.toggle('transformBar3');
     });
 }
 
