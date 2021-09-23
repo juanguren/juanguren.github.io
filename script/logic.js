3c02c3585176ec3eaec564212c80894477d1c402
Juan Felipe <juan_fe_7@live.com>
2021-09-23 15:58:13 -0500

    Minor updates

--- script/logic.js
+++ script/logic.js
@@ -1,37 +1,37 @@
 const responsiveNavBar = () => {
-  const nav = document.getElementById("hamburguer-icon");
-  const mobileMenu = document.getElementById("mobile-menu");
+  const nav = document.getElementById('hamburguer-icon');
+  const mobileMenu = document.getElementById('mobile-menu');
 
-  const bar1 = document.getElementsByClassName("bar1")[0];
-  const bar2 = document.getElementsByClassName("bar2")[0];
-  const bar3 = document.getElementsByClassName("bar3")[0];
+  const bar1 = document.getElementsByClassName('bar1')[0];
+  const bar2 = document.getElementsByClassName('bar2')[0];
+  const bar3 = document.getElementsByClassName('bar3')[0];
 
-  nav.addEventListener("click", () => {
-    mobileMenu.style.display === "none"
-      ? (mobileMenu.style.display = "block")
-      : (mobileMenu.style.display = "none");
+  nav.addEventListener('click', () => {
+    mobileMenu.style.display === 'none'
+      ? (mobileMenu.style.display = 'block')
+      : (mobileMenu.style.display = 'none');
 
-    bar1.classList.toggle("transformBar1");
-    bar2.classList.toggle("disappear");
-    bar3.classList.toggle("transformBar3");
+    bar1.classList.toggle('transformBar1');
+    bar2.classList.toggle('disappear');
+    bar3.classList.toggle('transformBar3');
   });
 };
 
 responsiveNavBar();
 
 const wordChangeinStart = () => {
-  let domSpace = document.getElementById("for-array");
+  let domSpace = document.getElementById('for-array');
   let counter = 0;
-  const wordArray = ["impact.", "good.", "fun."];
+  const wordArray = ['impact.', 'good.', 'fun.'];
   setInterval(() => {
     counter++;
     if (counter === 3) counter = 0;
 
     domSpace.textContent = wordArray[counter];
     wordArray.forEach(() => {
-      domSpace.classList.add("aboutWord");
+      domSpace.classList.add('aboutWord');
       setTimeout(() => {
-        domSpace.classList.remove("aboutWord");
+        domSpace.classList.remove('aboutWord');
       }, 1000);
     });
   }, 2200);
@@ -40,10 +40,10 @@ const wordChangeinStart = () => {
 wordChangeinStart();
 
 const emailButton = () => {
-  const email_button = document.querySelectorAll(".email-button");
+  const email_button = document.querySelectorAll('.email-button');
   email_button.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      location.href = "mailto:juan_fe_7@live.com";
+    btn.addEventListener('click', () => {
+      location.href = 'mailto:juanguren@pm.me';
     });
   });
 };
@@ -52,12 +52,12 @@ emailButton();
 
 const fixMobileNav = () => {
   const isDesktop = window.matchMedia(
-    "(min-width: 768px) and (max-width: 2000px)"
+    '(min-width: 768px) and (max-width: 2000px)'
   );
-  const mobileNav = document.getElementById("mobile-menu");
-  isDesktop.addEventListener("change", (e) => {
+  const mobileNav = document.getElementById('mobile-menu');
+  isDesktop.addEventListener('change', (e) => {
     if (e.matches) {
-      mobileNav.style.display = "none";
+      mobileNav.style.display = 'none';
     }
   });
 };
